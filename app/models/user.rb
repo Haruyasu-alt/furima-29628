ffa061108d9cfcefd49378fc499c8102b33a1fca
Haruyasu-alt <winnerisfree@gmail.com>
2021-01-16 11:55:01 +0900

    修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,17 @@ class User < ApplicationRecord
   validates :nickname
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
-  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
-  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
-  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
+
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
   validates :birthday
 end
 end
