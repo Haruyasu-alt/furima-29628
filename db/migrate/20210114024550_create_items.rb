54329fd2ba55f21a52ed29aa81f264158e492005
Haruyasu-alt <winnerisfree@gmail.com>
2021-01-17 20:37:58 +0900

    商品出品情報を保存するためのモデルとテーブルを作成

--- db/migrate/20210114024550_create_items.rb
+++ db/migrate/20210114024550_create_items.rb
@@ -2,7 +2,16 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
 
-      t.timestamps
+      t.string :name, null: false
+      t.text :explanation, null: false
+      t.integer :category_id, null: false
+      t.integer :state_id, null: false
+      t.integer :delivery_fee_id, null: false
+      t.integer :shipment_source_id, null: false
+      t.integer :days_to_ship_id, null: false
+      t.integer :price, null: false
+      t.references :user, foreign_key: true
+      
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,16 @@
 ActiveRecord::Schema.define(version: 2021_01_14_024550) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.text "explanation", null: false
+    t.integer "category_id", null: false
+    t.integer "state_id", null: false
+    t.integer "delivery_fee_id", null: false
+    t.integer "shipment_source_id", null: false
+    t.integer "days_to_ship_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -35,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_01_14_024550) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
